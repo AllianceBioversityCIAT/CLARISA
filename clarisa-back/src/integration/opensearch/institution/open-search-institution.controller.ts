32805dfc77b03b51975c8e2c90c5a42bfb65d6ba
German Martinez <g.martinez@cgiar.org>
2024-09-03 11:23:50 -0500

    ♻️refactor(OpenSearch): redocumenting endpoints with swagger

--- clarisa-back/src/integration/opensearch/institution/open-search-institution.controller.ts
+++ clarisa-back/src/integration/opensearch/institution/open-search-institution.controller.ts
@@ -10,19 +10,46 @@ import {
 import { JwtAuthGuard } from '../../../shared/guards/jwt-auth.guard';
 import { PermissionGuard } from '../../../shared/guards/permission.guard';
 import { OpenSearchInstitutionApi } from './open-search-institution.api';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiQuery,
+  ApiTags,
+} from '@nestjs/swagger';
 
 @Controller()
+@UseGuards(JwtAuthGuard, PermissionGuard)
+@ApiTags('OpenSearch')
 export class OpenSearchInstitutionController {
   constructor(private readonly openSearchApi: OpenSearchInstitutionApi) {}
 
   @Post('reset')
-  @UseGuards(JwtAuthGuard, PermissionGuard)
+  @ApiOperation({
+    summary:
+      'Reset the OpenSearch index, deleting all data and re-adding all institutions',
+  })
+  @ApiBearerAuth()
   async resetOpenSearch() {
     return this.openSearchApi.resetElasticData();
   }
 
   @Get('search')
-  @UseGuards(JwtAuthGuard, PermissionGuard)
+  @ApiQuery({
+    name: 'query',
+    required: true,
+    description: 'The query to search for (you can search by name or acronym)',
+  })
+  @ApiQuery({
+    name: 'sample-size',
+    required: false,
+    description: 'The number of results to return (by default 20)',
+    type: Number,
+  })
+  @ApiOperation({
+    summary:
+      'Search for institutions on the OpenSearch index. The returned data has the same schema as the /institutions endpoint, but with an additional property called "score". A higher score means a better match with the query.',
+  })
+  @ApiBearerAuth()
   async search(
     @Query('query') query: string,
     @Query('sample-size', new DefaultValuePipe(20), ParseIntPipe) size: number,
