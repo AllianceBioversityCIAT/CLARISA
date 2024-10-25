dd01bbff6792b66235c4cb2fd7be637552f2a4a2
German Martinez <g.martinez@cgiar.org>
2024-10-25 16:59:07 -0500

    ♻️refactor(Error): creating an http 500 error and modifying the 404 one
    - changing the entitynotfounderror to behave like factory
    - creating an internalservererror

--- clarisa-back/src/shared/errors/clarisa-entity-not-found.error.ts
+++ clarisa-back/src/shared/errors/clarisa-entity-not-found.error.ts
@@ -3,16 +3,61 @@ import { BaseHttpError } from './base-http-error';
 import { classNameCleaner } from '../utils/class-name-cleaner';
 
 export class ClarisaEntityNotFoundError<T> extends BaseHttpError<T> {
-  constructor(
+  /*private constructor(
     entityClass: string,
-    entityId: number | string,
+    paramValue: number | string | { [key: string]: number | string }[],
+    paramName: string = 'id',
     additionalData?: T,
   ) {
     super(
-      `A(n) ${classNameCleaner(entityClass)} with id "${entityId}" could not be found`,
+      `A(n) ${classNameCleaner(entityClass)} with ${paramName} = "${paramValue}" could not be found`,
       HttpStatus.NOT_FOUND,
     );
     this.name = 'EntityNotFoundError';
     this.additionalData = additionalData;
+  }*/
+
+  private constructor(message: string, additionalData?: T) {
+    super(message, HttpStatus.NOT_FOUND, additionalData);
+    this.name = 'EntityNotFoundError';
+  }
+
+  public static forSingleParam<T>(
+    entityClass: string,
+    paramName: string,
+    paramValue: number | string,
+    additionalData?: T,
+  ) {
+    return new ClarisaEntityNotFoundError(
+      `A(n) ${classNameCleaner(entityClass)} with ${paramName} = "${paramValue}" could not be found`,
+      additionalData,
+    );
+  }
+
+  public static forId<T>(
+    entityClass: string,
+    id: number | string,
+    additionalData?: T,
+  ) {
+    return ClarisaEntityNotFoundError.forSingleParam(
+      entityClass,
+      'id',
+      id,
+      additionalData,
+    );
+  }
+
+  public static forMultipleParams<T>(
+    entityClass: string,
+    params: { [key: string]: number | string }[],
+    additionalData?: T,
+  ) {
+    const paramsString = Object.entries(params)
+      .map(([key, value]) => `${key} = "${value}"`)
+      .join(', ');
+    return new ClarisaEntityNotFoundError(
+      `A(n) ${classNameCleaner(entityClass)} with params => "${paramsString}" could not be found`,
+      additionalData,
+    );
   }
 }
---
+++ clarisa-back/src/shared/errors/internal-server-error.ts
@@ -0,0 +1,14 @@
+import { HttpStatus } from '@nestjs/common';
+import { BaseHttpError } from './base-http-error';
+
+export class InternalServerError<T> extends BaseHttpError<T> {
+  //TODO add support email to the message
+  constructor(message: string, additionalData?: T) {
+    super(
+      `${message} Please contact us and let us know about this error message`,
+      HttpStatus.INTERNAL_SERVER_ERROR,
+    );
+    this.name = 'InternalServerError';
+    this.additionalData = additionalData;
+  }
+}
