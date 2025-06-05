09b6d8bb8523b2b1646280920f14bc7bd507b307
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-06-05 10:15:07 -0500

    :sparkles: feat: tracking-tools and clarity added

--- clarisa-front/src/app/app.component.ts
+++ clarisa-front/src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { TrackingToolsService } from './shared/services/tracking-tools.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(public trackingToolsService: TrackingToolsService) {}
+}
