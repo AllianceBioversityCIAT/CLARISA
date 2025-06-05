404c7b758d6c2f5e17dcab4a079be4898b991e27
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-06-05 10:28:06 -0500

    :white_check_mark: test: test removed

--- clarisa-front/src/app/app.component.spec.ts
+++ clarisa-front/src/app/app.component.spec.ts
@@ -20,11 +20,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'clarisa-front'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('clarisa-front');
-  });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
