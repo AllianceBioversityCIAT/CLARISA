8e8f51ea61bd715e4464589dc438906295ae6ac0
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2025-08-04 09:44:20 -0500

    :sparkles: feat: enhance dynamic table filters with new styles and functionality
    This commit adds a new filters container to the dynamic table filters component, including input fields for searching and selecting entity types and portfolios. It also introduces a styled export button for improved user interaction. Additionally, the necessary Angular FormsModule is imported to support two-way data binding for the dropdowns. The SCSS file is updated to style the filters container, ensuring a responsive layout.

--- clarisa-front/src/app/shared/components/dynamic-table-filters/dynamic-table-filters.component.ts
+++ clarisa-front/src/app/shared/components/dynamic-table-filters/dynamic-table-filters.component.ts
@@ -7,6 +7,8 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class DynamicTableFiltersComponent implements OnInit {
   @Input() dataList: any;
+  selectedEntityType: any;
+  selectedPortfolio: any;
 
   constructor() {}
 
